eae4a5fa6de1b6f0dfc8d8e7926ae8d83f779dd0
Stephen Higgins <stephen@gryphonandrook.com>
2024-12-06 14:28:40 -0700

    Initial gem structure for amplify_syndication

---
+++ lib/amplify_syndication.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "amplify_syndication/version"
+
+module AmplifySyndication
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/amplify_syndication/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AmplifySyndication
+  VERSION = "0.1.0"
+end
